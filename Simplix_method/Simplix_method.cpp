acfb5dac1d8239dc0278f988de76db913616b122
Savva <savva@loh.ru>
2024-02-21 11:43:53 +0300

    Initial commit

---
+++ Simplix_method/Simplix_method.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Simplix method\nInput Matrix size: ";
+    int n, m;
+
+    cin >> n;
+
+
+    vector<vector<int>> A(n - 2, vector<int>(n));
+
+
+
+}
